8cdb15545635b872ac8c2610f6c6c672e7c077f5
ETmanzer <163933890+ETmanzer@users.noreply.github.com>
2024-05-15 12:49:22 -0600

    added error handler

--- index.js
+++ index.js
@@ -63,6 +63,12 @@ app.get('/movies', (req, res) => {
     res.json(topMovies);
 });
 
+// error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
 // listen for requests
 app.listen(8080, () => {
     console.log('Your app is listening on port 8080.');
