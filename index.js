d0f3062f9f1cd7b92e416593e1a60833f80e0405
ETmanzer <163933890+ETmanzer@users.noreply.github.com>
2024-05-15 12:42:30 -0600

    added 10 movies

--- index.js
+++ index.js
@@ -1,2 +1,45 @@
 const express = require('express');
 const app = express();
+
+let topMovies = [
+    {
+        title: 'Terminator 2: Judegement Day',
+        director: 'James Cameron'
+    },
+    {
+        title: 'Star Wars: Episode V - The Empire Strikes Back',
+        director: 'Irvin Kershner'
+    },
+    {
+        title: 'Dances with Wolves',
+        director: 'Kevin Costner'
+    },
+    {
+        title: 'The Last Samuria',
+        director: 'Edward Zwick'
+    },
+    {
+        title: 'Dune',
+        director: 'Denis Villeneuve'
+    },
+    {
+        title: 'Dune: Part Two',
+        director: 'Denis Villeneuve'
+    },
+    {
+        title: 'Ladyhawke',
+        director: 'Richard Donner'
+    },
+    {
+        title: 'Star Wars: Episode VI - Return of the Jedi',
+        director: 'Richard Marquand'
+    },
+    {
+        title: 'John Wick',
+        director: 'Chad Stahelski'
+    },
+    {
+        title: 'John Wich: Chapter 2',
+        director: 'Chad Stahelski'
+    },
+];
