8ecbaeb02cae4f9fbd798678b63b90a1489c6c6a
ETmanzer <163933890+ETmanzer@users.noreply.github.com>
2024-06-05 05:57:30 -0600

    grammar correction

--- index.js
+++ index.js
@@ -291,7 +291,7 @@ app.get('/directors/:name', async (req, res) => {
             res.status(404).send('Director not found');
         }
     } catch (err) {
-        res.status 500).send('Error: ' + err);
+        res.status (500).send('Error: ' + err);
     }
 });
 
